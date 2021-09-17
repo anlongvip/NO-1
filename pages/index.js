230eb703c8e948f6213c09e804312344a8a0fefd
anlongvip <77271586+anlongvip@users.noreply.github.com>
2021-09-17 11:27:32 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://www.4everland.org/firstlanding/">4everland!</a>
         </h1>
 
         <p className={styles.description}>
